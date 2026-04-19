8d3627595dc02a3df64b4d4662e8867bde63b4bc
anup-08 <kumaranup31082004@gmail.com>
2026-04-19 23:41:07 +0530

    establish connection between db and service

---
+++ social-guardrail-service/src/main/java/com/example/guardrailService/SocialGuardrailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.guardrailService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialGuardrailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialGuardrailServiceApplication.class, args);
+	}
+
+}
---
+++ social-guardrail-service/src/test/java/com/example/guardrailService/SocialGuardrailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.guardrailService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialGuardrailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
